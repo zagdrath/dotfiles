569e687f8f4d3a6d13eaa1a8871f77721fdaaf96
Cody L. Wellman <cody@codexmicro.systems>
2022-09-20 13:40:27 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,6 @@ sudo apt install fonts-powerline
 # Copy dotfiles
 sudo cp -a $HOME/dotfiles/.config/* $HOME
 
-# Install vim plug
-curl -fLo $HOME/.config/nvim/site/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 # Install nerd fonts
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
 cd $HOME/dotfiles/nerd-fonts
