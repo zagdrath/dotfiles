027ed28e5e49038c10e08311b106929f019e0d5e
Cody L. Wellman <cody@codexmicro.systems>
2022-09-21 11:28:24 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Install dependencies
-sudo apt install git
-sudo apt install neovim
-sudo apt install fonts-powerline
+apt install git
+apt install neovim
+apt install fonts-powerline
 
 # Copy dotfiles
-sudo cp -a $HOME/dotfiles/.config/* $HOME
+cp -a $HOME/dotfiles/.config/* $HOME
 
 # Install nerd fonts
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
@@ -14,4 +14,4 @@ cd $HOME/dotfiles/nerd-fonts
 ./install.sh
 
 # Clean up files
-sudo rm -rf $HOME/dotfiles
+rm -rf $HOME/dotfiles
